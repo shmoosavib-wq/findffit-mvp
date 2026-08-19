ee7a43c7bfa264ded65172cc0ea5cbfe50ef227a
Hamid Moosavi <shmoosavib@gmail.com>
2026-08-19 17:14:41 +0330

    Add handlers index.js to fix module not found error

---
+++ src/bot/handlers/index.js
@@ -0,0 +1,9 @@
+﻿const startHandler = require('./start');
+const searchHandler = require('./search');
+const refineHandler = require('./refine');
+
+module.exports = {
+  startHandler,
+  searchHandler,
+  refineHandler
+};
